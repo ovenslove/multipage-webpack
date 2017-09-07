82d1f851c67ce4db093cf270116f08b779641679
ovenslove <1905997838@qq.com>
2017-09-07 16:33:49 +0800

    更新readme

--- src/view/index/index.js
+++ src/view/index/index.js
@@ -3,12 +3,5 @@ import './index.scss';
 import { test } from 'lib/utils';
 import axios from 'axios';
 
-axios.get('/api/4/news/latest')
-    .then(function(response) {
-        console.log(response);
-    })
-    .catch(function(error) {
-        console.log(error);
-    });
 console.log(test('aaa'));
 console.log("啊啊啊啊");
